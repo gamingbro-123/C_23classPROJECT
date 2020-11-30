a8f96077566366d8ce735ffc53c3f19b737dd05f
gamingbro-123 <73818869+gamingbro-123@users.noreply.github.com>
2020-11-30 14:07:41 +0530

    Add files via upload

---
+++ bin.js
@@ -0,0 +1,21 @@
+class bin
+{
+    constructer(xpos,ypos,width,height)
+    {
+        var options = {
+                           isStatic: true}
+
+       this.body = Bodies.rectangle(xpos,ypos,width,height,options);
+       this.width = width;
+       this.height = height;
+       World.add(world,this.body);
+    }
+
+    display()
+    {
+
+        rectMode(CENTER);
+        rect(this.body.position.x,this.body.position.y,this.width,this.height)
+    }
+
+}
